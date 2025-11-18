47fb9a2191cac3317100e5ee8f00201cc2cdf6f5
indra100x <hichemboutouatou@gmail.com>
2025-11-18 20:17:32 +0100

    finished

--- back-end/update_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once('bootstrap/app.php');
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-use Illuminate\Support\Facades\DB;
-
-DB::table('users')->where('email', 'hichemboutouatou@gmail.com')->update(['role' => 'admin']);
-echo "User role updated to admin\n";
